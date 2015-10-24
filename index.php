6ee886c09afc006a29ba888d4643c53ce8927892
Lewis Campbell <eccentaur@outlook.com>
2015-10-24 17:01:29 +0100

    A working index, I think it's brilliant, posts the start and finish point which can then be sent to Dom and Tom's method.

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+
+
+/*function getCoordsLat($location){
+    $loc = urlencode($location);
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address=$loc&sensor=false";
+    $json_result = json_decode(file_get_contents($url));
+
+    $geo_result =  $json_result->results[0];
+    $aCsv = $geo_result->geometry->location;
+
+    return $aCsv->lat;
+}
+
+function getCoordsLng($location){
+    $loc = urlencode($location);
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address=$loc&sensor=false";
+    $json_result = json_decode(file_get_contents($url));
+
+    $geo_result =  $json_result->results[0];
+    $aCsv = $geo_result->geometry->location;
+
+     return $aCsv->lng;
+}*/
+
+if(isset($_POST["S"]) OR isset($_POST["F"])) {
+    $start = $_POST["S"];
+    $finish = $_POST["F"];
+}
+
+?>
 <html>
     <head>
         <title>Homeward Bound</title>
@@ -24,16 +55,16 @@
     <body>
         <div class="app-bar darcula">
             <div class="makesmaller">
-    <a class="app-bar-element" href="...">Homeward Bound</a>
+                <a class="app-bar-element" href="...">Homeward Bound</a>
 
-    <span class="app-bar-divider"></span>
+                <span class="app-bar-divider"></span>
 
-    <ul class="app-bar-menu">
-        <li><a href="">About</a></li>
-        <li><a href="">Help</a></li>
-        <li><a href="">Contact</a></li>
-    </ul>
-        </div>
+                <ul class="app-bar-menu">
+                    <li><a href="">About</a></li>
+                    <li><a href="">Help</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
         </div>
         <div class="main padding20">
             <div class="call-to-action">
@@ -41,6 +72,22 @@
                 <div class=""
             </div>
             <div id="map" style="height:50%"></div>
+            <form autocomplete="off" action="" method="POST" role="form" enctype="multipart/form-data">
+                <label>Start</label>
+                <div class="input-control text">
+                    <input name="S" id="S" type="text">
+                </div>
+                <label>Finish</label>
+                <div class="input-control text">
+                    <input name="F" id="F" type="text">
+                </div>
+                <button type="submit"  class="button success block-shadow-success text-shadow">Button</button>
+            </form>
+        </div>
+        <div class="leader align-center">About</div>
+        <div>
+            <p>HomeWard Bound is a website app developed to get a person home via the safest route available to them, avoiding routes with a high level of crime rate in previous months.
+            Hopefully giving the person the safest route to there destination. Some more words to describe it....</p>
         </div>
     </body>
 </html>
